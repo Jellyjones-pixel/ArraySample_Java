0a69febe66e6c1c02f0ef3b16e73ce116731ac80
JOHN MBITHI MUTAVE <83606270+Jellyjones-pixel@users.noreply.github.com>
2023-09-27 17:01:38 +0100

    Add files via upload
    Updated

---
+++ ArraySample/src/arraysample/ArraySample.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arraysample;
+
+/**
+ *
+ * @author jelly_joneske
+ */
+public class ArraySample {
+
+    
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int x[] new int [5];
+    x [0] = 10;
+    x [1] = 20;
+    x [2] = 30;
+    x [3] = 40;
+    x [4] = 100;
+for(int 1=0;i<x.length;i++);
+System.out.println ("x[i]");
+ }
+    
+}
